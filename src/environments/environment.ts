82a30d254f184fa6cfa138346de305ca0c0b4f34
Ariel Chacon <mrarifav@gmail.com>
2026-03-07 21:42:25 -0600

    Nuevas imagenes publicitarias

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'http://localhost:5050',
   auth0: {
     domain: 'dev-e2c3vkx36p7pn2fl.us.auth0.com',
     clientId: 'qDzhn2fJeylcK6gdFlEycVvu9TMBhsag'
