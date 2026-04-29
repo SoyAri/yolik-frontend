a23616738ee53a39472a230c7eb1c3346d668e1a
chocotzi <a3523110609@alumno.uttehuacan.edu.mx>
2026-04-29 02:14:25 -0600

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'tienda',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'carrito',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'administracion',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'perfil',
     renderMode: RenderMode.Client
   },
   {
