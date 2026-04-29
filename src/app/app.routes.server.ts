40ac59d751dc0ad954fa35cec147f7c3c72c9dce
chocotzi <a3523110609@alumno.uttehuacan.edu.mx>
2026-04-29 00:39:29 -0600

    intento numero 500

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Client
   },
   {
+    path: 'detalle-producto/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
