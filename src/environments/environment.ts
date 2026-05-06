bcc4d69cfb80124b69f30f3dff5806404dc34fe2
chocotzi <a3523110609@alumno.uttehuacan.edu.mx>
2026-05-05 23:43:07 -0600

    cambio de credenciales auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   auth0: {
     domain: 'dev-yq7070sex2kql6cz.us.auth0.com',
     clientId: 'J4vVoVyo6CXtsmQAp0W2mSoYKjz9E3PO',
-    audience: 'http://localhost:5050',
+    audience: 'https://yolik.mx',
   },
   stripePublishableKey: 'pk_test_51TBet9K6FNEyqROJPZrMXzwOmWNPCuytCpeWp7CI6chLxFJz5Ji7ftlBjOKEf4ItjT1hJC3es5dvhXd8OgbAYOx300bvQEq9hp'
 };
